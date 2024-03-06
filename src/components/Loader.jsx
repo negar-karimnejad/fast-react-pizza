0c805cb980e2f21e39597d3aadc13c03bd5d4f65
negar <negar.karimnejad@gmail.com>
2024-03-06 15:04:37 +0330

    coding

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,6 +1,6 @@
 function Loader() {
   return (
-    <div className="absolute flex h-full w-full items-center justify-center bg-white/20 backdrop-blur-md">
+    <div className="absolute z-50 flex h-full w-full items-center justify-center bg-white/20 backdrop-blur-sm">
       <div className="loader"></div>
     </div>
   );
